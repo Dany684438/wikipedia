f013f60289bbfdf4551c7838b6589370fb6fe9af
Dany684438 <157891274+Dany684438@users.noreply.github.com>
2026-04-24 14:07:33 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,32 +1,38 @@
 from flask import Flask, request, jsonify
 import sqlite3
+from werkzeug.security import check_password_hash, generate_password_hash
 app = Flask(__name__)
 
-def init_db():
+def connect_db():
     conn = sqlite3.connect("data_base.db")
-    cursor = conn.cursor()
-    cursor.execute("""CREATE TABLE users(
+    conn.row_factory=sqlite3.Row
+    return conn
+
+def init_db():
+    conn = connect_db()
+    conn.execute("""
+        CREATE TABLE IF NOT EXIST users(
         id INT PRIMARY KEY AUTOINCREMENT,
         password VARCHAR(25),
-        login VARCHAR(20),
-        email VARCHAR(30),
-        phone_number VARCHAR(20)
+        login VARCHAR(20) UNIQUE,
+        email VARCHAR(30) UNIQUE,
+        phone_number VARCHAR(20) UNIQUE
         )
 
 
-        CREATE TABLE topics(
+        CREATE TABLE IF NOT EXIST topics(
                    id INT PRIMARY KEY AUTOINCREMENT,
                    description TEXT,
-                   name TEXT
+                   name TEXT UNIQUE
         )
-        CREATE TABLE articles(
+        CREATE TABLE IF NOT EXIST articles(
                    id INT PRIMARY KEY AUTOINCREMENT,
                    topics_id INT,
                    text TEXT
 
                    FOREIGN KEY (topics_id) REFERENCES topics(id)
         )
-        CREATE TABLE wikipedia(
+        CREATE TABLE IF NOT EXIST wikipedia(
                    id INT PROMARY KEY AUTOINCREMENT,
                    topics_id INT,
                    articles_id INT
@@ -36,8 +42,13 @@ def init_db():
         )
         
     """)
+    conn.execute("INSERT INTO topics (desciption, name) VALUES (?, ?)",
+                 ("Пример описания", "Наука"))
+    conn.execute("INSERT INTO topics (description, name) VALUES (?,?)",
+                 ("Пример описания", "История"))
     conn.commit()
     conn.close()
+    
 
 def profile():
     conn = sqlite3.connect("data_base.db")
@@ -61,18 +72,23 @@ def register():
     login = data['login']
     email = data['email']
     phone_number = data['phone_number']
-    conn = sqlite3.connect("data_base.db")
+
+    if not password or not login or not email:
+        return jsonify({
+            "message" : "Не все поля были заполнены"
+        })
+    
+    password_hash = generate_password_hash(password)
+
+    conn = connect_db()
     cursor = conn.cursor()
     cursor.execute("""INSERT INTO users(password, login, email, phone_number) VALUES(?, ?, ?, ?)""",
-                   (password, login, email, phone_number))
+                   (password_hash, login, email, phone_number))
     conn.commit()
     conn.close()
     return jsonify({
         "message" : "registration is ok"
     })
-
-
-
     
 
 @app.route("/login")
@@ -82,6 +98,9 @@ def login():
         return jsonify({
             "message" : "Поля не могут быть пустыми"
         }), 400
+
+
+
     password = data['password']
     email = data['email']
     if not "@" in email:
@@ -92,6 +111,9 @@ def login():
         return jsonify({
             "message" : "Пароль слишком короткий"
         }), 400
+
+
+        
     conn = sqlite3.connect("data_base.db")
     cur = conn.cursor()
     cur.execute("SELECT * FROM users WHERE email=?", (email,))
@@ -101,7 +123,8 @@ def login():
         return jsonify({
             "message" : "Пользователя не существует"
         }), 400
-    if not (password == user['password']):
+    # check_password_hash(пароль который пришел от польз, пароль из БД)
+    if not check_password_hash(user["password"], password):
         conn.close()
         return jsonify({
             "message" : "Пароль не верный"
@@ -114,13 +137,12 @@ def login():
 
 @app.route("/")
 def main():
-    conn = sqlite3.connect("data_base.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT name FROM topics")
-    name = cursor.fetchall()
+    conn = connect_db
+    conn.execute("SELECT name FROM topics")
+    name = conn.fetchall()
     return jsonify({
         "topics" : name
     })
-
+    conn.close()
 if __name__  == "__main__":
     app.run()
